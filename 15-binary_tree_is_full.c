b81c364c9032056ea6e8f48a2b3c69c2b797dd3b
be-great <begreat5333@gmail.com>
2024-06-03 15:19:31 +0300

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,7 +10,13 @@ int binary_tree_is_full(const binary_tree_t *tree)
     if (tree == NULL)
 		  return (0);
     
-
-
+    /* if we are leaf then it's full*/
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+    /*check every node by looping*/
+    if (tree->left != NULL && tree->right != NULL)
+        return (binary_tree_is_full(tree->left)
+        &&  binary_tree_is_full(tree->right));
+    /*else we are not full*/
     return (0);
 }
