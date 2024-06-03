9ab1c87f9f67e5760092ad1be973b23444b75f77
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-03 16:52:59 +0200

    fix 9-task betty style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,26 +1,24 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_height - function that measures the height of a node in a binary tree
+* binary_tree_height - function that measures the height of a node\
+* in a binary tree
 * @tree: the binary tree
 * Return: the height of the tree
 */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-		  return 0;
 
-    size_t lefth, righth;
-    /*divide*/
-    lefth = binary_tree_height(tree->left);
-    righth = binary_tree_height(tree->right);
-    if (lefth >= righth)
-    {
-        /*add it to previous call*/
-        return lefth + 1;
-    }
-    else
-        return righth + 1;
+	if (!tree)
+		return (0);
+	size_t leftSubTree, rightSubTree;
 
+	leftSubTree = binary_tree_height(tree->left);
+	rightSubTree = binary_tree_height(tree->right);
+
+	if (leftSubTree > rightSubTree)
+		return (leftSubTree + 1);
+	else
+		return (rightSubTree + 1);
 }
