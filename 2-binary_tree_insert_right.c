f0cb0a98f920208e9b4bce7f711061117d51bb7a
be-great <begreat5333@gmail.com>
2024-06-03 13:06:48 +0300

    merge

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,7 +8,7 @@
  *
  * Return: Pointer to the created node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
