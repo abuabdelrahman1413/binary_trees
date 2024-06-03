289d16e6da59ee2fcc7742d5d958357d6c6c3c20
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-03 11:09:54 +0200

    update 0-task

---
+++ .ccls-cache/@home@mohammed@Projects@binary_trees/0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - creates a binary tree node
+* @parent: pointer to the parent node
+* @value: value to put in the new node
+*
+* Return: pointer to the new node
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	return (newNode);
+}
