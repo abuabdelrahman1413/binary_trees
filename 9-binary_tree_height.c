88a2738e40bf57fefa2e61938a6946e6f447397c
be-great <begreat5333@gmail.com>
2024-06-03 13:27:05 +0300

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,6 +8,19 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    if (tree == NULL)
+		return 0;
 
+    int lefth, righth;
+    /*divide*/
+    lefth = binary_tree_height(tree->left);
+    righth = binary_tree_height(tree->right);
+    if (lefth > righth)
+    {
+        /*add it to previous call*/
+        return lefth + 1;
+    }
+    else
+        return righth + 1;
 
 }
