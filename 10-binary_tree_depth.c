c2e02187fd92ec4fbdc2f6aa773d1a9691aafbad
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-04 14:48:43 +0200

    update 12-task

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,9 +9,9 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	if (tree->parent == NULL)
+	if (!tree->parent)
 		return (0);
 	return (1 + binary_tree_depth(tree->parent));
 }
