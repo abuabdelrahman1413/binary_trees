93ea66b96c084403adfba6555edf3b39e1a60d96
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-04 12:11:17 +0200

    update 1-task

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,13 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+		return (NULL);
+
 	binary_tree_t *newNode;
 
 	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	if (newNode == NULL || parent == NULL)
+	if (newNode == NULL)
 		return (NULL);
 	newNode->parent = parent;
 	newNode->n = value;
