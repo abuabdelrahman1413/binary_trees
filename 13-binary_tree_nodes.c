c8bf62e1e1bc23b750edd8348fda7e7852fdc562
be-great <begreat5333@gmail.com>
2024-06-04 12:45:14 +0300

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,6 +7,14 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t count = 0;
 
+	if (tree == NULL)
+		return (0);
 
+	if (tree->left != NULL || tree->right != NULL)
+		count = 1;
+
+	return (count + binary_tree_nodes(tree->left) +
+	binary_tree_nodes(tree->right));
 }
