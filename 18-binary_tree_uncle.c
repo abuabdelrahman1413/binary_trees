88e23c0e502313dccb3ee43280de4aa89d19e240
be-great <begreat5333@gmail.com>
2024-06-05 19:42:03 +0300

    fix task 18 bug

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,16 +7,11 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *parent, *grandparent;
-
-	parent = node->parent;
-	grandparent = parent->parent;
-
-	if (grandparent->left == parent)
-		return (grandparent->right);
-	else
-		return (grandparent->left);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
