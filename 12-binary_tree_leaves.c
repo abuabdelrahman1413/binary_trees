bfdffc7165bfde5e419cc1a38edd87f77f1b0fa6
be-great <begreat5333@gmail.com>
2024-06-05 19:53:50 +0300

    fix task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,7 +7,7 @@
  * Return: 1 if node is a leaf, 0 otherwise
  */
 
-size_t binary_tree_nodes(const binary_tree_t *tree)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
@@ -24,12 +24,14 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leftSubTree = 0, rightSubTree = 0;
+
 	if (!tree)
 		return (0);
 	if (binary_tree_is_leaf(tree))
 		return (1);
-	size_t leftSubTree = binary_tree_leaves(tree->left);
-	size_t rightSubTree = binary_tree_leaves(tree->right);
+	leftSubTree = binary_tree_leaves(tree->left);
+	rightSubTree = binary_tree_leaves(tree->right);
 
 	return (leftSubTree + rightSubTree);
 
