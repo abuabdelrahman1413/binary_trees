6f57c09099ecb22b5647768675b8ef3f5ad6e65a
be-great <begreat5333@gmail.com>
2024-06-05 19:08:46 +0300

    merge

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,7 +7,7 @@
  * Return: 1 if node is a leaf, 0 otherwise
  */
 
-int binary_tree_is_leaf(const binary_tree_t *node)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (!node)
 		return (0);
