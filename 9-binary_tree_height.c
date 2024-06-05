f4ddebd183b5022f39140bfc2f64f32f6a5d5563
be-great <begreat5333@gmail.com>
2024-06-05 18:49:17 +0300

    bitch task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,20 +9,25 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t leftSubTree = 0, rightSubTree = 0;
 
-	if (!tree)
-		return (0);
-	/*if the node is the leaf height is 0*/
-	if (!tree->left && !tree->right)
+	if (tree == NULL)
 		return (0);
 
-	size_t leftSubTree, rightSubTree;
-
-	leftSubTree = binary_tree_height(tree->left);
-	rightSubTree = binary_tree_height(tree->right);
-
-	if (leftSubTree >= rightSubTree)
-		return (leftSubTree + 1);
+	if (tree != NULL)
+	{
+		if (tree->left != NULL)
+			leftSubTree = 1 + binary_tree_height(tree->left);
+		else
+			leftSubTree = 0;
+		if (tree->right != NULL)
+			rightSubTree = 1 + binary_tree_height(tree->right);
+		else
+			rightSubTree = 0;
+	}
+	if (leftSubTree > rightSubTree)
+		return (leftSubTree);
 	else
-		return (rightSubTree + 1);
+		return (rightSubTree);
+
 }
